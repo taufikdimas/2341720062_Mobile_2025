fc2bda89dc43671c4fb288634e5af7304245fc19
taufikdimas <taufikdimasedystara@gmail.com>
2025-11-08 04:07:01 +0700

    W11: Soal 3

--- Week-11/src/books_27/lib/main.dart
+++ Week-11/src/books_27/lib/main.dart
@@ -45,7 +45,21 @@ class _FuturePageState extends State<FuturePage> {
         child: Column(
           children: [
             const Spacer(),
-            ElevatedButton(onPressed: () {}, child: Text('Go!')),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+                getData()
+                    .then((value) {
+                  result = value.body.toString().substring(0, 450);
+                  setState(() {});
+                })
+                    .catchError((_) {
+                  result = 'An error occurred';
+                  setState(() {});
+                });
+              },
+              child: Text('Go!'),
+            ),
             const Spacer(),
             Text(result),
             const Spacer(),
